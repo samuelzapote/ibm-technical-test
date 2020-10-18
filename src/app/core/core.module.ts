c39a31d4f0a02d702b0d46002fc2065d7f477d5d
Samuel Zapote <samuelzapote@outlook.com>
2020-10-18 17:49:44 -0500

    feat(Core): created the core module
    along with the layout hoc and its header child component.

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from './hoc/header/header.component';
+import { LayoutComponent } from './hoc/layout/layout.component';
+
+@NgModule({
+	declarations: [HeaderComponent, LayoutComponent],
+	imports: [
+		CommonModule
+	],
+	exports: [
+		LayoutComponent,
+	],
+})
+export class CoreModule { }
