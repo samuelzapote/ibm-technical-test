b2ce447256d8200ea812cc5d40f6a890cf82331a
Samuel Zapote <samuelzapote@outlook.com>
2020-10-23 16:19:48 -0500

    feat(env): Added search delay ms

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
 	production: true,
 	BASE_GITHUB_API_URL: 'https://api.github.com',
 	DEFAULT_AVATAR_URL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+	SEARCH_DELAY_MS: 1250,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,7 @@ export const environment = {
 	production: false,
 	BASE_GITHUB_API_URL: 'https://api.github.com',
 	DEFAULT_AVATAR_URL: 'https://material.angular.io/assets/img/examples/shiba1.jpg',
+	SEARCH_DELAY_MS: 1250,
 };
 
 /*
